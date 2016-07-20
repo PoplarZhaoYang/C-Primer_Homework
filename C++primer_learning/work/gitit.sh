cba14af5d9d5e0e47ab5515794f4c55234005f73
jibancanyang <1599664856@qq.com>
2016-07-20 21:09:37 +0800

    jibancanyang

---
+++ C++primer_learning/work/gitit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git add .
+git commit -m "jibancanyang"
+git push origin master
+
+echo "git operator is completed!"
+
+
+
--- C++primer_learning/work/zy.cpp
+++ C++primer_learning/work/zy.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <string>
+#include <iterator>
+int A[] = {1, 3, 7, 2, 8, 0, 5, 0, 5};
 
 int main()
 {
-    using std::cin;
-    using std::string;
-    using std::endl;
-    using std::cout;
-    string str(10, 'a');
-    str += string(10, 'b');
-    cout << str << endl;
-    for (char c : str) c = 'x';
-    //没有改变，因为c是拷贝的新变量
-    cout << str << endl;
+    auto head = std::begin(A), tail = std::end(A);
+    while (head != tail) {
+        *head = 0;
+        ++head;
+    }
+    for (head = std::begin(A), tail = std::end(A);  head != tail; ++head) {
+        std::cout << *head << std::endl;
+    }
     return 0;
 }
